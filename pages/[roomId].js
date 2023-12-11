f6fb60acdaba57813b34a86b898c325ed9aa6b1d
evoxf1 <sahil2812@live.com>
2023-12-11 17:39:54 +0530

    added player to play stream

--- pages/[roomId].js
+++ pages/[roomId].js
@@ -1,14 +1,18 @@
 import { useSocket } from "@/context/socket";
+import useMediaStream from "@/hooks/useMediaStream";
 import usePeer from "@/hooks/usePeer";
+
+import Player from "@/components/Player";
 const Room = () => {
   const socket = useSocket();
   const { peer, myId } = usePeer();
+  const { stream } = useMediaStream();
+
+  return (
+    <div>
+      <Player url={stream} muted playing playerId={myId} />
+    </div>
+  );
 };
 
-export default Room
-
-// useEffect(() => {
-//     socket?.on("connect", () => {
-//       console.log(socket.id);
-//     });
-//   }, [socket]);
+export default Room;
